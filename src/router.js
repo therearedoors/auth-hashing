41c5dd21e09f39a7e21c95deeddc70eb68f4beb5
Paul Smart <pdsmart6@gmail.com>
2022-04-11 18:27:54 +0100

    requirements complete

--- src/router.js
+++ src/router.js
@@ -7,11 +7,35 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    
+    const {username,password} = req.body
+    if (!username) return res.status(400).json({error: 'no username'})
+    if (!password) return res.status(400).json({error: 'no password'})
+    const hash = await bcrypt.hash(password, 10)
+    const result = await prisma.user.create({
+        data: {
+            username: username,
+            password: hash
+        }
+    })
+    res.json(result)
 });
 
-router.post('/login', async (req, res) => {
-    
+router.post('/login', async (req, res) => {4
+    const {username,password} = req.body
+    if (!username) return res.status(400).json({error: 'no username'})
+    if (!password) return res.status(400).json({error: 'no password'})
+    const hash = await bcrypt.hash(password,10)
+    const result = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+        })
+    if (!result) return res.status(404).json({error: 'user not found'})
+    const auth = await bcrypt.compare(password,result.password)
+    if (!auth) return res.status(400).json({error: 'invalid password'})
+    const token = await jwt.sign(username,'secret')
+    res.json({token: token})
+
 });
 
 module.exports = router;
